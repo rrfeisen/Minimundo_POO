9397d3f7c0ecddd70bed6aa5a2ca563e73d22fec
Roberto Ramos <56175688+rrfeisen@users.noreply.github.com>
2025-07-19 00:44:47 -0300

    Update Professor.java

--- CRUD_Basico/entity/Professor.java
+++ CRUD_Basico/entity/Professor.java
@@ -16,4 +16,59 @@ public class Professor extends Entidade {
     @Column(columnDefinition = "DATE")
     private LocalDate nascimento;
 
+    public Professor() {
+    }
+
+    public Professor(String nome, String cpf, LocalDate nascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.nascimento = nascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "id=" + getId() +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", nascimento=" + nascimento +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Professor professor = (Professor) o;
+        return Objects.equals(getId(), professor.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
 }

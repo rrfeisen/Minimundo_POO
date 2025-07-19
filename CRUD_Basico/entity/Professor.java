9cc9e4e7a7db1a1f304554190b25db50f41a34cb
Roberto Ramos <56175688+rrfeisen@users.noreply.github.com>
2025-07-19 00:46:07 -0300

    Update Professor.java

--- CRUD_Basico/entity/Professor.java
+++ CRUD_Basico/entity/Professor.java
@@ -2,6 +2,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "professor")
